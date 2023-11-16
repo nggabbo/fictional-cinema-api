6d01b53daa74f11fbcd272ab4bdb02ff4e7c9e57
nggabbo <g4briel.ruiz@gmail.com>
2023-11-16 16:49:39 -0300

    feat: :sparkles: create users module, make user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ default: 'user' })
+  role: string;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
