8f0e99bf2dc90a878fdaefed061617ba2d850076
nggabbo <g4briel.ruiz@gmail.com>
2023-11-16 16:16:31 -0300

    build: :heavy_plus_sign: add nestjs/typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'gab',
+      password: 'g4b',
+      database: 'fictional_cinema',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
